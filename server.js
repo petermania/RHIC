38e1901fe017a9ddfca653ad07b3e07327862b00
petermania <scott@incrediblemachines.net>
2016-11-15 16:12:50 -0500

    dded listener

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.get('/', function (req, res) {
 app.get('/inbound', function (req, res) {
     console.log('inbound text received')
     inbounds++
+    console.log(req)
     res.render(
         'index',
         { title: 'INBOUND SMS', message: 'Inbound Number '+inbounds+' Received!'})
