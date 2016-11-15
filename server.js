6b4c51b003aadb0d4fda5f771ffc3d00a86e7421
petermania <scott@incrediblemachines.net>
2016-11-15 16:24:04 -0500

    SPELLING ERROR

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/inbound', function (req, res) {
     console.log(req.query.xml)
     parseString(req.query.xml, function (err, result) {
         console.dir(result)
-        console.log(reault["phonenumber"])
+        console.log(result["phonenumber"])
     });
     res.render(
         'index',
