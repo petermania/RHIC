94c846f09cdb699669314e10e8534b80893003cd
petermania <scott@incrediblemachines.net>
2016-11-15 23:30:07 -0500

    Bootstrap Front End

--- server.js
+++ server.js
@@ -4,18 +4,40 @@ var http=require('http')
 
 var inbounds=0
 
-
-
 var xml2js = require('xml2js')
 
-
+var fs = require("fs")
 
 app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
-  res.render(
-      'index',
-      { title: 'INBOUND SMS', message: 'Inbound Number '+inbounds+' Received!'})
+
+
+  console.log("\n *START* \n")
+  var content = fs.readFileSync("data.json")
+  console.log("Output Content : \n"+ content)
+  console.log("\n *EXIT* \n")
+
+  var results=JSON.parse(content)
+  var arr = []
+
+  for(var x in results){
+    arr.push(results[x])
+  }
+
+  arr.sort(function (a, b) {
+    if (a.order > b.order) {
+      return 1;
+    }
+    if (a.order < b.order) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+
+  console.dir(arr)
+  res.render('index',{results : arr, title : 'Test'})
 })
 
 app.get('/inbound', function (req, res) {
@@ -35,5 +57,5 @@ app.get('/inbound', function (req, res) {
 })
 
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Listening on port 8080!')
 })
