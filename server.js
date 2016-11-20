cb82ea428792c5e225ad33c3db8dfa2423c41200
petermania <scott@incrediblemachines.net>
2016-11-20 10:34:34 -0800

    receiver

--- server.js
+++ server.js
@@ -116,6 +116,7 @@ app.get('/inbound', function (req, res) {
           col.find({status:'active'}).toArray(function(err,actRes){
             if(actRes){
               if(json.TRUMPIA.CONTENTS.includes(actRes[0].yes_text)){
+                console.log("yes received")
                 col.updateOne({poll_id:parseInt(actRes[0].query.poll_id)},
                   {$set: {yes_vote:actRes[0].yes_vote++}},
                   {upsert:false},
