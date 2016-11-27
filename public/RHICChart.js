0e99a9d6be917990fe8aa640619e017733240612
petermania <scott@incrediblemachines.net>
2016-11-27 18:05:46 -0500

    Commiting new image loading attempt for viewer

--- public/RHICChart.js
+++ public/RHICChart.js
@@ -1,5 +1,10 @@
-// var server_location='http://localhost:8080'
-var server_location='http://54.173.181.162:8080/'
+var server_location='http://localhost:8080'
+// var server_location='http://54.173.181.162:8080/'
+
+var sky=new Image()
+var grass=new Image()
+var wood=new Image()
+var colors=[]
 
 window.chartOptions = {
   segmentShowStroke: false,
@@ -58,21 +63,10 @@ socket.on('update',function(data){
   chartUpdate()
 })
 
-var chartUpdate = function() {
-  Chart.defaults.global.tooltips.enabled = false;
-  var input=[]
-  var colors=[]
-  var labels=[]
-
-
-  console.log("Updating Chart")
-
-  var sky = new Image()
-  sky.src = 'sky.png'
-  var grass = new Image()
-  grass.src = 'grass.png'
-  var wood = new Image()
-  wood.src = 'wood.png'
+var loadImages = function(callback){
+  sky.src='sky.png'
+  grass.src='grass.png'
+  wood.src='wood.png'
   // Replace the chart canvas element
   $('#chart').replaceWith('<canvas id="chart" width="650" height="650"></canvas>')
   // Draw the chart
@@ -87,45 +81,59 @@ var chartUpdate = function() {
         var grassPattern = ctx.createPattern(grass,'repeat');
         var skyPattern = ctx.createPattern(sky,'repeat');
         if(response_no>3){
-          input.push(vote4)
           colors.push(grassPattern)
-          labels.push(text4.toUpperCase())
         }
         if(response_no>2){
-          input.push(vote3)
           colors.push(woodPattern)
-          labels.push(text3.toUpperCase())
         }
-        input.push(vote2)
         colors.push(skyPattern)
-        labels.push(text2.toUpperCase())
 
-        input.push(vote1)
         colors.push(grassPattern)
-        labels.push(text1.toUpperCase())
+        callback()
+      }
+    }
+  }
+}
 
-        console.log(colors)
-        var info = {
-          datasets: [{
-              data: input,
-              backgroundColor:colors,
-              label: 'Total Votes' // for legend
-          }],
-          labels: labels
-        }
+var chartUpdate = function() {
+  Chart.defaults.global.tooltips.enabled = false;
+  var input=[]
+  var labels=[]
+  // Draw the chart
+  var ctx = $('#chart').get(0).getContext("2d")
+  if(response_no>3){
+    input.push(vote4)
+    labels.push(text4.toUpperCase())
+  }
+  if(response_no>2){
+    input.push(vote3)
+    labels.push(text3.toUpperCase())
+  }
+  input.push(vote2)
+  labels.push(text2.toUpperCase())
 
+  input.push(vote1)
+  labels.push(text1.toUpperCase())
 
+  console.log(colors)
+  var info = {
+    datasets: [{
+        data: input,
+        backgroundColor:colors,
+        label: 'Total Votes' // for legend
+    }],
+    labels: labels
+  }
 
-        var myPieChart = new Chart(ctx,{
-          type: 'doughnut',
-          data: info,
-          options: chartOptions
-        })//new Chart
-      }//wood onload
-    }//grass onload
-  }//sky onload
+  var myPieChart = new Chart(ctx,{
+    type: 'doughnut',
+    data: info,
+    options: chartOptions
+  })//new Chart
 }//function
 
 $(document).ready(function() {
-  chartUpdate()
+  loadImages(function(){
+      chartUpdate()
+  })
 })
